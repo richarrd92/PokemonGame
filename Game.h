8695be1c3c70730bc45b14addba4154e14231c02
Richard Maliyetu <160991580+richarrd92@users.noreply.github.com>
2024-04-03 11:20:00 -0400

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,19 +1,4 @@
-/*****************************************
-** File:    Game.h
-** Project: CMSC 202 Project 2, Spring 2024
-** Author:  Richard Maliyetu
-** Date:    3/11/24
-** Section: 33
-** E-mail:  r215@gl.umbc.edu 
-** E-mail:  r215@umbc.edu 
-**
-** This file contains the declaration of the game Pokemon
-** This program loads a file 
-** Data in the file is stored into an array. this array is used to fill m_pokeDex array
-** m_pokeDex array is used to create a team of 4 pokemon that are used to battle against other pokemon
-** the 4 pokemon can be trained up to max combat power and so much more
-** more info in the project description.
-***********************************************/
+
 #ifndef GAME_H
 #define GAME_H
 #include "Pokemon.h"
